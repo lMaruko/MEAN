324260cc9cb1075d32cbb51168751fb8426acad4
Maruko <38086766+zlMaku@users.noreply.github.com>
2021-01-29 11:16:02 -0500

    FEAT:Médicos y Hospitales

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -6,13 +6,15 @@ import {
   SharedService,
   UsuarioService,
   LoginGuardGuard,
-  SubirArchivoService
+  SubirArchivoService,
+  HospitalService,
+  MedicoService
 } from './service.index';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalUploadService } from '../components/modal-upload/modal-upload.service';
 @NgModule({
   declarations: [],
-  providers: [SettingsService, SidebarService, SharedService, UsuarioService,LoginGuardGuard, SubirArchivoService,ModalUploadService],
+  providers: [SettingsService, SidebarService, SharedService, UsuarioService,LoginGuardGuard, SubirArchivoService,ModalUploadService,HospitalService,MedicoService],
   imports: [CommonModule, HttpClientModule],
 })
 export class ServiceModule {}
